c9fcdd8ce8877aced03e15a9c51af458273600ab
Shubham-2004 <shubhambera2004@gmail.com>
2025-09-17 23:38:30 +0530

    [feat]: Initial backend project setup

---
+++ sweetshop-backend/src/main/java/com/sweetshop/sweetshop_backend/SweetshopBackendApplication.java
@@ -0,0 +1,22 @@
+package com.sweetshop.sweetshop_backend;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SweetshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetshopBackendApplication.class, args);
+	}
+
+	@RestController
+	public static class HelloController {
+		
+		@GetMapping("/")
+		public String hello() {
+			return "Hello World!";
+		}
+	}
+}
